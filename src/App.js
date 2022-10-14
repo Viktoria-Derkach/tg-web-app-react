333aad9e3a9a8749e81e459aefdd1a45b5035a50
ViktoriiaDerkach <viktoriia.derkach@airslate.com>
2022-10-14 19:38:23 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
+
+import './App.css';
+import React, {useEffect} from "react";
+// import {useTelegram} from "./hooks/useTelegram";
+
+const tg = window.Telegram.WebApp
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // const {onToggleButton, tg} = useTelegram();
+
+    useEffect(() => {
+        tg.ready();
+    }, [])
+
+    const onClose = () => {
+      tg.close()
+    }
+
+    return (
+        <div className="App">
+          hhfjfg
+       <button onClick={onClose}>Close</button>
+        </div>
+    );
 }
 
 export default App;
