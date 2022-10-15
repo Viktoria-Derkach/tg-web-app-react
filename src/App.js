b10e45ae88db8bee60f9ca2635af353abcc4c858
ViktoriiaDerkach <viktoriia.derkach@airslate.com>
2022-10-15 11:57:05 +0300

    hook

--- src/App.js
+++ src/App.js
@@ -3,22 +3,19 @@
 import './App.css';
 import React, {useEffect} from "react";
 import Header from './components/Header/Header';
-// import {useTelegram} from "./hooks/useTelegram";
+import {useTelegram} from "./hooks/useTelegram";
 
-const tg = window.Telegram.WebApp
 function App() {
-    // const {onToggleButton, tg} = useTelegram();
+    const {onToggleButton, tg} = useTelegram();
 
     useEffect(() => {
         tg.ready();
-    }, [])
-
-
+    }, [tg])
 
     return (
         <div className="App">
           <Header />
-          work
+          <button onClick={onToggleButton} >Toggle</button>
         </div>
     );
 }
