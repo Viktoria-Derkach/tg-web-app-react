eee439b62d5bdbf2107d99ba89ec4fb36407896a
ViktoriiaDerkach <viktoriia.derkach@airslate.com>
2022-10-15 14:03:58 +0300

    use

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,20 +1,24 @@
 const tg = window.Telegram.WebApp;
 
-export const useTelegram = () => {
-  const onClose = () => {
-    tg.close()
-  }
-  const onToggleButton = () => {
-    if (tg.MainButton.isVisible) {
-      tg.MainButton.hide()
-    } else {
-      tg.MainButton.show()
+export function useTelegram() {
+
+    const onClose = () => {
+        tg.close()
     }
-  }
-  return {
-    onClose,
-    onToggleButton,
-    tg,
-    user: tg.initDataUnsafe?.user,
-  }
+
+    const onToggleButton = () => {
+        if(tg.MainButton.isVisible) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }
+
+    return {
+        onClose,
+        onToggleButton,
+        tg,
+        user: tg.initDataUnsafe?.user,
+        queryId: tg.initDataUnsafe?.query_id,
+    }
 }
